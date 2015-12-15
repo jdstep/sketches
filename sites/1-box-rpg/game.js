29e3d76eae4b536fcd9d5190d83b0d48e04297a0
jd <jdstep23@gmail.com>
2015-12-15 14:37:47 -0800

    monster image appears

--- sites/1-box-rpg/game.js
+++ sites/1-box-rpg/game.js
@@ -1,5 +1,24 @@
 
+var enemies = {
+  "Counter": {
+    hp: 5,
+    img: 'http://www.11points.com/images/animatedgifs/counter.gif'
+  }
+
+}
+
+var loadEnemy = function(name) {
+  name = "Counter";
+  var $enemyStats = $('.enemy.stats');
+  var $enemyImg = $('.enemy-img');
+  var enemyData = enemies[name];
+
+  $enemyImg.attr('src', enemyData.img);
+}
+
+
 $(document).ready(function() {
   var $root = $('.root');
-  
+
+  loadEnemy();
 });
