945d8c30c59eb00a263fcf0fac9cecb543efefed
jd <jdstep23@gmail.com>
2015-12-15 14:51:02 -0800

    player options html/css

--- sites/1-box-rpg/game.js
+++ sites/1-box-rpg/game.js
@@ -1,24 +1,33 @@
 
 var enemies = {
   "Counter": {
+    name: "Counter",
     hp: 5,
     img: 'http://www.11points.com/images/animatedgifs/counter.gif'
   }
 
 }
 
-var loadEnemy = function(name) {
-  name = "Counter";
-  var $enemyStats = $('.enemy.stats');
-  var $enemyImg = $('.enemy-img');
-  var enemyData = enemies[name];
 
-  $enemyImg.attr('src', enemyData.img);
-}
 
 
 $(document).ready(function() {
   var $root = $('.root');
+
+  var loadEnemy = function(name) {
+    name = "Counter";
+    var $enemyStats = $('.enemy.stats');
+    var $enemyImg = $('.enemy-img');
+    var enemyData = enemies[name];
+    var $enemyName = $('.enemy-name');
+    var $enemyHp = $('.enemy-hp');
+
+    $enemyImg.attr('src', enemyData.img);
+    $enemyName.html('<h3>' + enemyData.name + '</h3>');
+    $enemyHp.html('<h2>' + enemyData.hp + '</h2>');
+
+  }
+  
 
   loadEnemy();
 });
